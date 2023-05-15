1a4dfbc207189067cca875102d59527444c60941
ved-khanna <70955356+ved-khanna@users.noreply.github.com>
2023-05-15 12:55:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pro86
+# spectagram-stage-2
+project solution for c82
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDOOt0jmaNUS19FwdxkaaQaCRl_Aht5w0U",
+    authDomain: "spectagram-app-11f30.firebaseapp.com",
+    projectId: "spectagram-app-11f30",
+    storageBucket: "spectagram-app-11f30.appspot.com",
+    messagingSenderId: "662479808228",
+    appId: "1:662479808228:web:9237984bdef3881488aef1"
+  };
